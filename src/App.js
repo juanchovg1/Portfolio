c407c903c9af95b3b800467c090ca6b4029e15a7
Juan Villota <villota99@hotmail.com>
2024-10-17 21:58:47 -0500

    Refactor: Modularized the app by creating separate components for better structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ProjectList from './components/ProjectList';
+import ContactForm from './components/ContactForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>JUAN</h1>
-      </header>
+      <Header />
+      <ProjectList />
+      <ContactForm />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className="App-header">
+            <h1>JUAN</h1>
+        </header>
+    );
+    }
+    
+    export default Header;
